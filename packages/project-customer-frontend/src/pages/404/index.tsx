0d697265ee176760d7947bd6cfb3c98ba8bb90c5
endersonlg <endersonluciog@gmail.com>
2024-12-09 23:07:47 -0300

    feat: 404 page

---
+++ packages/project-customer-frontend/src/pages/404/index.tsx
@@ -0,0 +1,21 @@
+import { Box } from '@/components/box'
+import Link from 'next/link'
+
+export default function Custom404() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <Box className="relative text-center">
+        <h2 className="text-4xl font-bold text-gray-750 mb-4">
+          404 - Page Not Found
+        </h2>
+        <p className="text-base text-gray-700 mb-6">
+          Oops! The page you&apos;re looking for doesn&apos;t exist. It might
+          have been moved or deleted.
+        </p>
+        <Link href={'/'} className="text-blue-500 hover:underline font-medium">
+          Go Back to Homepage
+        </Link>
+      </Box>
+    </main>
+  )
+}
