ed4b29ed573bf569f7e87c946ebfa8d1c91052de
endersonlg <endersonluciog@gmail.com>
2024-12-10 13:16:45 -0300

    fix: env and dockerfile

--- packages/project-customer-frontend/src/libs/axios.ts
+++ packages/project-customer-frontend/src/libs/axios.ts
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-const backendUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://project-customer-monorepo.onrender.com'
-    : process.env.NEXT_PUBLIC_BACKEND_URL
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
 
 export const api = axios.create({
   baseURL: backendUrl,
