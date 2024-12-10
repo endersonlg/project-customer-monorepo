aa76579519c11266cad77933a6f906c1e030ffdf
endersonlg <endersonluciog@gmail.com>
2024-12-10 14:51:09 -0300

    test: 8

--- packages/project-customer-frontend/src/libs/axios.ts
+++ packages/project-customer-frontend/src/libs/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
+const backendUrl =
+  process.env.NEXT_PUBLIC_BACKEND_URL ||
+  'https://project-customer-monorepo.onrender.com'
 
 export const api = axios.create({
   baseURL: backendUrl,
