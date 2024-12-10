147ab994f33bd4ac0a53f532fc8e7faa334d4db7
endersonlg <endersonluciog@gmail.com>
2024-12-10 09:51:45 -0300

    test: vercel 3

--- packages/project-customer-frontend/src/pages/404/index.tsx
+++ packages/project-customer-frontend/src/pages/404/index.tsx
@@ -1,9 +1,17 @@
 import { Box } from '@/components/box'
+import { getColors } from '@/http/get-colors'
 import Link from 'next/link'
 
 export default function Custom404() {
+  async function teste() {
+    console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
+    const response = await getColors()
+    console.log(response)
+  }
+
   return (
     <main className="flex items-center justify-center h-screen">
+      <button onClick={teste}>teste</button>
       <Box className="relative text-center">
         <h2 className="text-4xl font-bold text-gray-750 mb-4">
           404 - Page Not Found
