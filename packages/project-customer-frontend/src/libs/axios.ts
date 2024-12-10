1b263a9c38f11261ffa8169a7f1aa8740976122d
endersonlg <endersonluciog@gmail.com>
2024-12-10 11:19:44 -0300

    test: vercel 5

--- packages/project-customer-frontend/src/libs/axios.ts
+++ packages/project-customer-frontend/src/libs/axios.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
+const backendUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://project-customer-monorepo.onrender.com'
+    : process.env.NEXT_PUBLIC_BACKEND_URL
 
 export const api = axios.create({
   baseURL: backendUrl,
